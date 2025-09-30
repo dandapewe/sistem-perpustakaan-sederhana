e2130a00c9f0dd888eaa1fff0a1cfd3390d55276
Danda Prayogi Wijaya <dandaprayogiwijaya29@gmail.com>
2025-09-30 21:03:20 +0700

    Create /src/main/java/com/praktikum/testing/repository/RepositoriBuku.java

---
+++ src/main/java/com/praktikum/testing/repository/RepositoriBuku.java
@@ -0,0 +1,22 @@
+package com.praktikum.testing.repository;
+
+import com.praktikum.testing.model.Buku;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface RepositoriBuku {
+    boolean simpan(Buku buku);
+
+    Optional<Buku> cariByIsbn(String isbn);
+
+    List<Buku> cariByJudul(String judul);
+
+    List<Buku> cariByPengarang(String pengarang);
+
+    boolean hapus(String isbn);
+
+    boolean updateJumlahTersedia(String isbn, int jumlahTersediaBaru);
+
+    List<Buku> cariSemua();
+}
